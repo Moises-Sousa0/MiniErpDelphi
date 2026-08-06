6320abf9eb92c2548d999d01b9b8db9b02b075a9
Moises-Sousa0 <moisessousanow@gmail.com>
2026-08-05 22:56:53 -0300

    atualizartela

---
+++ __recovery/uFrameCaixa.pas
@@ -0,0 +1,79 @@
+unit uFrameCaixa;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, DAO.Caixa, Model.Caixa, uFrmPrincipal,
+  Vcl.StdCtrls;
+
+type
+  TFrameCaixa = class(TFrame)
+    lblStatusCaixa: TLabel;
+    EdtValorAbertura: TEdit;
+    btnAbrirCaixa: TButton;
+    btnFecharCaixa: TButton;
+    lblCaixaInfo: TLabel;
+    procedure btnAbrirCaixaClick(Sender: TObject);
+  private
+    { Private declarations }
+    FCaixaDao: TCaixaDao;
+    FCaixaAtual: TCaixa;
+    procedure AtualizarTela;
+
+  public
+    { Public declarations }
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+  end;
+
+
+implementation
+
+{$R *.dfm}
+
+
+
+constructor TFrameCaixa.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FCaixaDao := TCaixaDao.Create;
+  AtualizarTela;
+end;
+
+
+destructor TFrameCaixa.Destroy;
+begin
+  FCaixaDao.Free;
+  inherited Destroy;
+end;
+
+
+procedure TFrameCaixa.AtualizarTela;
+begin
+  FCaixaAtual := FCaixaDao.BuscarCaixaAberto;
+  if FCaixaAtual = nil then
+  begin
+    lblStatusCaixa.Visible := True;
+    btnAbrirCaixa.Visible := True;
+    btnFecharCaixa.Visible := False;
+    lblCaixaInfo.Visible := False;
+    EdtValorAbertura.Visible := True;
+  end
+
+  else
+  begin
+    lblStatusCaixa.Visible := True;
+    btnFecharCaixa.Visible := True;
+    lblCaixaInfo.Visible := True;
+    btnAbrirCaixa.Visible := False;
+    EdtValorAbertura.Visible := False;
+  end;
+end;
+
+procedure TFrameCaixa.btnAbrirCaixaClick(Sender: TObject);
+begin
+
+end;
+
+end.
